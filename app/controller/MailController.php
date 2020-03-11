51dfcea4b6c1cb9f4bf883f14c927832c9a33d90
Didier Page <didier.page@heig-vd.ch>
2020-03-11 19:05:13 +0100

    <fix><captcha> remove useless captcha check

--- app/controller/MailController.php
+++ app/controller/MailController.php
@@ -7,20 +7,18 @@ Class MailController{
     private $password;
     private $host;
     private $port;
-    private $recaptcha;
 
 
-    public function __construct($username, $password,$recaptchaSecret,$port,$host){
+    public function __construct($username, $password,$port,$host){
         $this->username = $username;
         $this->password = $password;
         $this->host = $host;
         $this->port = $port;
-        $this->recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret);
     }
 
     public function sendMail($data,$response,$slimView,$viewfile, $to){
-        $resp = $this->recaptcha->verify($data['g-recaptcha-response']);
-        if ($resp->isSuccess()) {
+
+
             // Create the Transport
             $transport = (new \Swift_SmtpTransport($this->host,  $this->port))
                 ->setUsername( $this->username)
@@ -35,12 +33,7 @@ Class MailController{
                 ->setBody($slimView->render($response,"mail/".$viewfile, ['data' => $data])->getBody(),'text/html');
 
             $result = $mailer->send($message);
-            var_dump($result);
-        }else{
-            $errors = $resp->getErrorCodes();
-            var_dump($errors);
-        }
-
+     
         return $response;
 
     }
